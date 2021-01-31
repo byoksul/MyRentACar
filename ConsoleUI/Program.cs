2cc0ede551c918f9274003bde1a3c210b6398360
Burak Yoksul <359144@ogr.ktu.edu.tr>
2021-02-01 01:28:13 +0300

    RentACar Güncel

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+
+            foreach (var car in carManager.GetAll()) 
+            {
+                Console.WriteLine("----BY YOKSUL RENT A CAR--- \n ---Araç Özellikleri---" );
+                Console.WriteLine("AraçID:" + car.CarId + "MarkaID:" + car.BrandId + "Renk:" + car.ColorId + "Model Yılı:" + car.ModelYear + "Günlük Fiyatı:" + car.DailyPrice + "Açıklama:" + car.Description);
+
+            }
+
+        }
+    }
+}
